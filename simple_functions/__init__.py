87fd3cc1cc805c5205aff2814bdcbd21f026e8ce
rhodrin <rhodri.nelson@gmail.com>
2020-10-09 17:06:31 +0100

    Add basics.

---
+++ simple_functions/__init__.py
@@ -0,0 +1,8 @@
+from simple_functions.functions1 import *
+
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass

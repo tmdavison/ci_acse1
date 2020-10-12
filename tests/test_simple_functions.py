b8f422c88dcb3289d2b1526ded2cca1cd76dac36
rhodrin <rhodri.nelson@gmail.com>
2020-10-12 10:52:41 +0100

    Tweaks 1.

--- tests/test_simple_functions.py
+++ tests/test_simple_functions.py
@@ -8,8 +8,8 @@ class TestSimpleFunctions(object):
     '''Class to test our simple functions are working correctly'''
 
     @pytest.mark.parametrize('iterable, expected', [
-        ('[8, 7, 5]', '20'),
-        ('(10, -2, 5, -10, 1)', '4')
+        ([8, 7, 5], 20),
+        ((10, -2, 5, -10, 1), 4)
     ])
     def test_my_add(self, iterable, expected):
         '''Test our add function'''
